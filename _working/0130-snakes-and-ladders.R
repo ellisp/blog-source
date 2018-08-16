f82c97a8e1bef7e97e5cb6843c028414b9913b52
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-08-16 18:49:01 +0530

    snakes and ladders ok with extra roll for dice

--- _working/0130-snakes-and-ladders.R
+++ _working/0130-snakes-and-ladders.R
@@ -6,34 +6,36 @@ library(knitr)
 # TODO - this ignores the fact that you get another roll if you get a six
 
 test_m <- function(M){
-  probs <- apply(M, 1, sum) 
+  probs <- round(apply(M, 1, sum), 6) 
   if(sum(probs == 1) != nrow(M)){
     message(paste("rows: ", paste(which(probs !=1 ), collapse = ", ")))
     print(probs[probs != 1])
-    stop("Some rows of transition probabilities don't sum to 1")
+    stop("Some rows of transition probabilities don't sum to 1.000000")
   }
 }
 
 n <- 10 * 10
 
 # we have n + 1 different states (because position zero is "off the board"), each of which 
-# has a probability of transitioning to another. To start with, w say they can transition to 
-# n +1 + 5 (105 in the case of a 10 x 10 board, as if you roll 99 you go first 10 105)
-M <- matrix(0, n + 1, n + 1 + 6)
+# has a probability of transitioning to another. 
+M <- matrix(0, n + 1, n + 1 )
+colnames(M) <- 0:n
+rownames(M) <- 0:n
 
 # The row is the position at the start of the round; the column is the position at the end; 
 # the cell value is the probability of transitioning from the row to the column
 
-for(i in 1:6){
-  diag(M[ , (i + 1):(i+1+n)]) <- 1 / 6
+for(i in 1:n){
+  M[i , (i + 1):min(n + 1, (i + 6))] <- 1 / 6
 }
 
-# add in in the "bounce back"s
-for(i in 1:6){
-  M[, n + 1 - i] <- M[, n + 1 - i] + M[ , n + 1  + i] # If you end up on square 101 (i=102), you go down to square 99 (i = 100)
+# add in in the "bounce back"s - you stay in your own square if you don't roll exactly what you need to get to 100
+for(i in (n - 4):(n + 1)){
+  M[i, i] <- 1- sum(M[i , -i])
 }
+# This also nicely covers the case of once you are on square 100 (state 101), you stay there forever
 
-M <- M[1:(n + 1), 1:(n + 1)]
+M[90:101, 90:101]
 
 M_orig <- M
 test_m(M_orig)
@@ -76,10 +78,6 @@ snakes_and_ladders <- data_frame(
 
 M <- M_orig
   
-if(length(starting) != length(ending)){
-  stop("different number of starting and ending points for snakes and ladders")
-  }
-
 # For each snake/ladder
 for(i in 1:(nrow(snakes_and_ladders))){
   sl_start_col <- snakes_and_ladders$starting[i] + 1
@@ -102,14 +100,6 @@ for(i in 1:(nrow(snakes_and_ladders))){
   }
   test_m(M)
 }
-
-# If you get to row 100, you stay there:
-M[n + 1, ] <- 0
-M[n + 1, n + 1] <- 1
-colnames(M) <- 0:n
-rownames(M) <- 0:n
-
-
 
 #------------visualise board-------------------
 nrows <- sqrt(n)
@@ -197,9 +187,6 @@ ggplot(res_df, aes(x = turns2, y = prob_not_finished)) +
   geom_line() +
   scale_x_log10(breaks = c(1, 3, 10, 30, 100, 300)) +
   theme(panel.grid.minor.x = element_blank())
-View(res_df)
-
-sum(M[101 , -101])
 
 
 turns3 <- c(0:300)
@@ -225,3 +212,106 @@ dist_df3 %>%
   filter(square == 100) %>%
   mutate(prob_won = c(0, diff(prob)))  %>%
   summarise(expected_win_time = sum(prob_won * turns))
+# mean number of turns to win is 39.2, without the extra 6 rule which matches the JSTOR article
+# *with* the extra sixes mean number of turns to win goes down to 32.9
+
+#--------------when 6 means another turn------------
+# this means that the transition matrix isn't quite right.  For instance, M[1, 7:]
+M[1, 7]
+# suggests you have a 1/6 chance of finishing on 6 at the end of your first go, when in 
+# fact you have zero chance.  So the procedure would be:
+# for each starting state
+# 1. identify where you end with a six and reduce that by 1/6
+# 2. note the transition probabilities to other states from that point as the starting point
+# 3. add those transition probabilities, divided by 6 to the row of our current starting state - *except*
+#    for the 6 from there (ie if we roll 6-6-6) which becomes a zero, and instead every row gets a 1/(6^3)
+#    probability of going back to square 1.
+#
+# This should be done before adding in the snakes and ladders, so we'll start again:
+
+M <- matrix(0, n + 1, n + 1 )
+
+# basic chances 1/6 for 1, 2, 3, 4, 5 or 6 squares
+for(i in 1:n){
+  M[i , (i + 1):min(n + 1, (i + 6))] <- 1 / 6
+}
+
+# add in in the "bounce back"s - you stay in your own square if you don't roll exactly what you need to get to 100
+for(i in (n - 4):(n + 1)){
+  M[i, i] <- 1- sum(M[i , -i])
+}
+
+# add in the possibility of getting sixes and hence another roll:
+# We do this backwards because of the chance of people getting 2+ sixes in a row
+for(i in (n-7):1){
+  # Not possible to end up here from rolling 6 (TODO what if it is the winning roll):
+  M[i, i + 6] <- M[i, i + 6] - 1/6
+  
+  # Instead we get 1/6th of the chances of what can be done from the end point
+  v <- M[i +6, ]
+  M[i, ] <- M[i, ] + v / 6
+}
+
+# still not accounted for:
+# - rolling 3 sixes goes back to square one (and in some variants, then you must roll six again to start)
+
+
+# TODO - simulation; including maybe an animation
+
+sims <- list()
+reps <- 1000
+for(i in 1:reps){
+  current_state  <- c(1, rep(0, n))
+  all_states <- 0
+  
+  while(current_state[n + 1] != 1){
+    new_probs <- as.vector(current_state %*% M)
+    current_state <- as.vector(rmultinom(1, 1, new_probs))
+    all_states <- c(all_states, which(current_state == 1) - 1)
+    }
+  
+  sims[[i]] <- all_states
+}
+
+# dir.create("0130-tmp")
+
+for(i in 1:100){
+  this_sim <- data_frame(square = sims[[i]]) %>%
+    mutate(turn = 1:n()) %>%
+    left_join(board, by = c("square" = "sequence"))
+  
+  p <- ggplot(board, aes(y = board_row, x = board_col)) +
+    geom_tile(colour = "white", fill = "steelblue") +
+    
+    geom_curve(data = sl_df, aes(y = board_row.x, x = board_col.x,
+                                 yend = board_row.y, xend = board_col.y,
+                                 colour = type),
+               arrow = arrow(type = "closed", length = unit(0.1, "inches")),
+               curvature = 0.2, size = 2) +
+    geom_text(aes(label = sequence)) +
+    
+    geom_path(data = this_sim, colour = "red", size = 2, arrow = arrow()) +
+    
+    scale_colour_manual(values = c(Ladder = "white", Snake = "orange")) +
+    
+    theme_void(base_family = "Roboto")  +
+    theme(legend.position = "none", 
+          plot.title = element_text(family = heading_font) ) +
+    coord_equal() +
+    ggtitle("Simulated snakes and ladders games")
+  
+  png(paste0("0130-tmp/", 1000000 + i, ".png"), 6 * 200, 6 * 200, res = 200)
+    print(p)
+  dev.off()
+}
+
+pd <- setwd("0130-tmp")
+system('magick -loop 0 -delay 40 *.png "0130-snakes-and-ladders.gif"')
+setwd(pd)
+
+lengths <- data.frame(x = sapply(sims, length))
+
+ggplot(lengths, aes(x= x)) +
+  geom_density(fill = "blue", alpha = 0.2) +
+  labs(x = "Number of moves before winning") +
+  ggtitle("Distribution of length of one player snakes and ladders games")
