fe45511c9bfd4ff286bfef09bff61f8334f98948
Peter Ellis <peter.ellis2013nz@gmail.com>
2021-06-14 11:53:02 +1000

    working version of penguins PC illustration

---
+++ _working/0202-principal-penguins.R
@@ -0,0 +1,55 @@
+library(palmerpenguins)
+library(tidyverse)
+
+penguins2 <- drop_na(penguins) %>%
+  mutate(row_id = 1:n())
+
+m1 <- penguins2 %>%
+  select(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g) 
+
+pc1 <- prcomp(m1, scale = TRUE)
+penguins2$pc1 <- rescale(predict(pc1)[, 1])
+
+penguins2 %>%
+  ggplot(aes(x = body_mass_g, y = pc1, colour = species)) +
+  geom_point() +
+  labs(x = "Body mass in grams",
+       y = "First principal component\n(rescaled from zero to 1)",
+       caption = "Example data: Palmer Penguins",
+       colour = "Species:",
+       title = "First principal component of four-dimensional data",
+       subtitle = "Fit to data in its original row order")
+
+
+m2 <- penguins2 %>%
+  select(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, row_id) %>%
+  arrange(desc(flipper_length_mm))
+
+pc2 <- prcomp(select(m2, -row_id), scale = TRUE)
+
+penguins3 <- m2 %>%
+  mutate(pc2 = rescale(predict(pc2)[, 1])) %>%
+  select(row_id, pc2) %>%
+  inner_join(penguins2, by = "row_id") 
+
+
+penguins3 %>%
+  ggplot(aes(x = body_mass_g, y = pc2, colour = species)) +
+  geom_point() +
+  labs(x = "Body mass in grams",
+       y = "First principal component\n(rescaled from zero to 1)",
+       caption = "Example data: Palmer Penguins",
+       colour = "Species:",
+       title = "First principal component of four-dimensional data",
+       subtitle = "Fit to data in a different row order")
+
+
+penguins3 %>%
+  ggplot(aes(x = pc1, y = pc2, colour = species)) +
+  geom_point() +
+  labs(x = "When fit to data in original row order",
+       y = "When fit to data in a different row order",
+       title = "The direction of a principal component can vary based on the data's arrangement",
+       subtitle = "Comparison of the first principal component, scaled from 0 to 1, fit to the same data",
+       caption = "Example data: Palmer Penguins",
+       colour = "Species:")
