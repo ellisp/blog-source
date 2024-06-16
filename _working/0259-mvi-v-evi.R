ef999f817a9b0459978d82d10ca5e97cf27a31c0
Peter Ellis <petere@spc.int>
2024-06-16 15:33:01 +1100

    correct subject tags

--- _working/0259-mvi-v-evi.R
+++ _working/0259-mvi-v-evi.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 
-#source("0257-mvi-again.R")
+# source("0257-mvi-again.R")
 
 download.file("https://www.un.org/development/desa/dpad/wp-content/uploads/sites/45/LDC_data.xlsx",
               destfile = "LDC_data.xlsx", mode = "wb")
@@ -62,9 +62,6 @@ d <- mvi   |>
             by = "country") |>
   drop_na()
 
-library(Cairo)
-CairoWin()
-
 #------------find a line that divides the data into two---------------
 # We want a backwards diagonal line that goes through the medians and has
 # half the countries on each side - so we can split into those that are
@@ -100,3 +97,58 @@ p <- d |>
        y  = "New proposed multidimensional vulnerability index")
 
 svg_png(p, "../img/0258-unctad-v-mvi", w = 10, h = 7)
+
+
+#-------------------------------v ND-GAIN----------------------------
+download.file("https://gain.nd.edu/assets/521644/nd_gain_country_index_2023.zip",
+              destfile = "nd_gain_2023.zip", mode = "wb")
+unzip("nd_gain_2023.zip", overwrite = TRUE)
+
+
+ndgain <- read_csv("resources/vulnerability/vulnerability.csv") |>
+  gather(year, value, -ISO3, -Name)
+
+
+
+nd_mvi <- mvi   |>
+  select(country = Country, ISO, new_mvi = `MVI - Score`, is_pict) |>
+  full_join(filter(ndgain, year == 2021), by = c("ISO" = "ISO3")) |>
+  drop_na() |>
+  rename(ndgain_vul = value) |>
+  mutate(is_pict = is_pict == "Pacific Island")
+
+#------------find a line that divides the data into two---------------
+# We want a backwards diagonal line that goes through the medians and has
+# half the countries on each side - so we can split into those that are
+# 'more poor than vulnerable' and vice versa
+center <- (c(median(nd_mvi$ndgain_vul, na.rm = TRUE), median(nd_mvi$new_mvi, na.rm = TRUE)))
+
+test_slope <- function(b){
+  a <- center[2] - b * center[1]
+  more_poor <- mean((a + b * nd_mvi$ndgain_vul) < nd_mvi$new_mvi)
+  return(abs(0.5 - more_poor))
+}
+test_slope(0)
+# optim doesn't work here so we just use brute force, try a big range of plausible vlaues
+tests <- tibble(possible_slopes = seq(from = 5, to = 20, length.out = 1000)) |>
+  mutate(disc = Vectorize(test_slope)(possible_slopes)) |>
+  arrange(disc)
+
+# get the actual slope and intercept that work best
+b <- tests[1, ]$possible_slopes
+a <- center[2] - b * center[1]
+
+p <- nd_mvi |>
+  ggplot(aes(x = ndgain_vul, y = new_mvi, colour = is_pict)) +
+  geom_abline(intercept = a, slope = b, colour = "grey20") +
+  geom_vline(xintercept = median(nd_mvi$ndgain_vul, na.rm = TRUE), colour = "grey80") +
+  geom_hline(yintercept = median(nd_mvi$new_mvi, na.rm = TRUE), colour = "grey80") +
+  geom_point() +
+  geom_text_repel(data = filter(nd_mvi, is_pict), aes(label = country), alpha = 0.5) +
+  scale_colour_manual(values = c("grey", "blue")) +
+  theme(panel.grid = element_blank(), legend.position = "none") +
+  labs(x = "ND GAIN vulnerability index",
+       y  = "New proposed multidimensional vulnerability index",
+       subtitle = "Diagonal line splits the data in two; countries below / to the right are more vulnerable on the ND GAIN measure.")
+
+svg_png(p, "../img/0258-ndgain-v-mvi", w = 10, h = 7)
