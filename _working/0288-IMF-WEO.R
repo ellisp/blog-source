c25d17934b507c3e1998cddd0c67eea29962c79f
Peter Ellis <petere@spc.int>
2025-04-24 10:16:03 +1100

    rationalise object names and don't download files that have already been downloaded

--- _working/0288-IMF-WEO.R
+++ _working/0288-IMF-WEO.R
@@ -12,29 +12,27 @@ library(tidyverse)
 #----------------download sdmx version-------------------------
 options(timeout=600)
 
-# couldn't get the tab delimited version to work - encoding problems - so went
-# with the SDMX version
-download.file("https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/2025/april/WEOAPR2025-SDMXData.ashx",
-              destfile = "weo2025-apr.zip", mode = "wb")
+dluz <- function(url, destfile){
+  if(!file.exists(destfile)){
+    download.file(url, destfile = destfile, mode = "wb")
+    unzip(destfile)
+  }
+}
 
-# this update only seems to be available for selected countries:
-download.file("https://www.imf.org/-/media/Files/Publications/WEO/2025/update/january/english/data/weo-update-january-2025.ashx",
-              destfile = "weo2025-jan.xlsx", mode = "wb")
 # to see the outlooks where the whole database is available, all countries, see
 # https://www.imf.org/en/Publications/SPROLLS/world-economic-outlook-databases#sort=%40imfdate%20descending
 
-download.file("https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/2024/October/WEOOCT2024-SDMXData.ashx",
-              destfile = "weo2024-oct.zip", mode = "wb")
+dluz("https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/2025/april/WEOAPR2025-SDMXData.ashx",
+              destfile = "weo2025-apr.zip")
 
-unzip("weo2025-apr.zip")
-unzip("weo2024-oct.zip")
 
-read_excel("weo2025-jan.xlsx")
+dluz("https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/2024/October/WEOOCT2024-SDMXData.ashx",
+              destfile = "weo2024-oct.zip")
 
 #---------------processing---------------
 
 library(rsdmx)
-d <- readSDMX("WEOAPR2025/xmlfile_APR2025.xml", isURL = FALSE) |> 
+d2025 <- readSDMX("WEOAPR2025/xmlfile_APR2025.xml", isURL = FALSE) |> 
   # this parsing takes a long time:
   as_tibble()
 
@@ -42,14 +40,14 @@ d2024 <- readSDMX("WEOOCT2024/WEO_PUB_OCT2024.xml", isURL = FALSE) |>
   # this parsing takes a long time:
   as_tibble()
 
-count(d, SCALE)    # 1, 1000000 and 1000000000
-count(d, CONCEPT)  # BCA, BCA_NGDP, BF, BFD, etc
-count(d, UNIT)     # B to U
+count(d2025, SCALE)    # 1, 1000000 and 1000000000
+count(d2025, CONCEPT)  # BCA, BCA_NGDP, BF, BFD, etc
+count(d2025, UNIT)     # B to U
 count(d2024, UNIT)     # B to U
-count(d, REF_AREA)     # 001, 110, etc
-count(d, FREQ)                # everying is A (annual)
+count(d2025, REF_AREA)     # 001, 110, etc
+count(d2025, FREQ)                # everying is A (annual)
 count(d2024, FREQ)            # same
-count(d, LASTACTUALDATE)      # varies, from 2003 and higher
+count(d2025, LASTACTUALDATE)      # varies, from 2003 and higher
 
 concept <- read_xlsx("WEOAPR2025/WEOPUB_DSD_APR2025.xlsx", sheet = "CONCEPT", skip = 7)[, 1:2] |> 
   rename(CONCEPT = Code,
@@ -185,10 +183,10 @@ weo2025 |>
   count(concept)
 
 # lots of things in USD:
-weo2025 |> 
-  filter(unit == "U.S. dollars") |> 
-  count(concept, CONCEPT) |> 
-  View()
+# weo2025 |> 
+#   filter(unit == "U.S. dollars") |> 
+#   count(concept, CONCEPT) |> 
+#   View()
 
 
 
