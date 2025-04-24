ea199ca32b03ae4f9f3453eac5f5217817a6dbd7
Peter Ellis <petere@spc.int>
2025-04-25 10:44:13 +1100

    reorder charts code

--- _working/0288-IMF-WEO.R
+++ _working/0288-IMF-WEO.R
@@ -128,7 +128,133 @@ stopifnot(all(regions %in% ref_areas$country))
 
 
 
-#----------------charts----------------------------
+#=============================charts===========================
+
+#-------------headline year on year growth rates------------
+
+# from trial and error on which series ("CONCEPT") to use, we see the figures
+# used for headline is real GDP growth so constant prices, but not per capita
+growth_comps <- weo_both |> 
+  filter(CONCEPT == "NGDP_R") |> 
+  filter(year %in% 2024:2025) |> 
+  group_by(country, edition) |> 
+  summarise(growth = value[year == 2025] / value[year == 2024] - 1) |> 
+  ungroup()
+
+selected <- c(
+  "United States",
+  "Canada",
+  "Japan",
+  "United Kingdom",
+  "Germany",
+  "Italy",
+  "France",
+  "China",
+  "India",
+  "Russia",
+  "Brazil"
+)
+
+bgcol <- "grey90"
+
+p0 <- growth_comps |> 
+  filter(country %in% selected) |> 
+  mutate(country = factor(country, levels = selected[length(selected):1])) |> 
+  mutate(text_col = ifelse(growth < 0, "negative", "positive")) |> 
+  ggplot(aes(x = growth, y = country, fill = edition)) +
+  geom_col(position = "dodge", width = 0.75, colour = bgcol) +
+  geom_text(aes(label = percent(growth, accuracy = 0.1), colour = text_col), 
+            position = position_dodge(width = 0.7), hjust = 1, vjust = 0.5,
+            fontface = "bold", size = 3) +
+  scale_fill_manual(values = c("darkblue", "steelblue"), guide = guide_legend(reverse = TRUE)) +
+  scale_colour_manual(values = c("positive" = "white", "negative" = "black")) +
+  scale_x_continuous(label = percent) +
+  geom_vline(xintercept = 0) +
+  guides(colour = "none") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        plot.background = element_rect(fill = bgcol, colour = NA),
+        axis.line.y = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = c(0.8, 0.8)) +
+  labs(fill = "",
+       x = "",
+       y = "",
+       title = "Projected real GDP growth in 2025 for selected nations",
+       subtitle = "Forecasts for 2025, made in October 2024 and April 2025 in the IMF World Economic Outlook.
+The update for some, but not all, countries in January 2025 is not considered here.",
+       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
+
+
+
+p1 <- growth_comps |> 
+  filter(country %in% pacific) |> 
+  mutate(country = fct_reorder(country, -growth, .fun = mean)) |> 
+  mutate(text_col = ifelse(growth < 0, "negative", "positive")) |> 
+  ggplot(aes(x = growth, y = country, fill = edition)) +
+  geom_col(position = "dodge", width = 0.75, colour = bgcol) +
+  geom_text(aes(label = percent(growth, accuracy = 0.1), colour = text_col), 
+            position = position_dodge(width = 0.7), hjust = 1, vjust = 0.4,
+            fontface = "bold", size = 3) +
+  scale_fill_manual(values = c("darkblue", "steelblue"), guide = guide_legend(reverse = TRUE)) +
+  scale_colour_manual(values = c("positive" = "white", "negative" = "black")) +
+  scale_x_continuous(label = percent) +
+  geom_vline(xintercept = 0) +
+  guides(colour = "none") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        plot.background = element_rect(fill = bgcol, colour = NA),
+        axis.line.y = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = c(0.8, 0.8)) +
+  labs(fill = "",
+       x = "",
+       y = "",
+       title = "Projected real GDP growth in 2025 for Pacific IMF members",
+       subtitle = "Forecasts for 2025, made in October 2024 and April 2025 in the IMF World Economic Outlook.",
+       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
+
+svg_png(p0, "../img/00288-selected-growth", w = 8, h = 7)
+
+svg_png(p1, "../img/00288-pict-growth", w = 8, h = 7)
+
+
+gcw <- growth_comps |> 
+  spread(edition, growth) |> 
+  mutate(is_pict = ifelse(country %in% pacific, "Pacific", "Other"))
+
+
+p2 <- gcw |> 
+  ggplot(aes(x = `WEO October 2024`, y = `WEO April 2025`, colour = is_pict)) +
+  geom_abline(slope = 1, intercept = 0, colour = "grey75") +
+  geom_point() +
+  geom_text_repel(data = filter(gcw,
+                                `WEO April 2025` > 0.10 |
+                                  `WEO October 2024` < 0 |
+                                  `WEO October 2024` > 0.08 | 
+                                  is_pict == "Pacific"),
+                  aes(label = country),
+                  seed = 123) +
+  theme(legend.position = "none") +
+  coord_equal() +
+  scale_x_continuous(label = percent) +
+  scale_y_continuous(label = percent) +
+  scale_colour_manual(values = c("Pacific" = "blue", "Other" = "grey60")) +
+  annotate("text", x = 0.2, y = 0.04, label = "Forecast lower in April 2025", fontface = "italic") +
+  annotate("text", x = 0, y = 0.12, label = "Forecast higher in April 2025", fontface = "italic") +
+  labs(x = "Forecast as at October 2024",
+       y = "Forecast as at April 2025",
+       title = "Changes in IMF forecasts of real GDP growth for 2025, by country",
+       subtitle = "Pacific island countries highlighted. Diagonal line shows the same forecast in both 2024 and 2025.",
+       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
+
+
+# note tried transform_modulus but it seems not to work?
+
+svg_png(p2, "../img/00288-pict-growth-scatter", w = 8, h = 6)
+
+#--------------------growth time series--------------------------
+
 
 weo2025 |> 
   filter(CONCEPT == "NGDPRPPPPC") |> 
@@ -294,126 +420,4 @@ weo2025 |>
   labs(title = ctxt,
        subtitle = utxt)
 
-#-------------headline growth rates------------
-
-# from trial and error on which series ("CONCEPT") to use, we see the figures
-# used for headline is real GDP growth so constant prices, but not per capita
-growth_comps <- weo_both |> 
-  filter(CONCEPT == "NGDP_R") |> 
-  filter(year %in% 2024:2025) |> 
-  group_by(country, edition) |> 
-  summarise(growth = value[year == 2025] / value[year == 2024] - 1) |> 
-  ungroup()
-
-selected <- c(
-  "United States",
-  "Canada",
-  "Japan",
-  "United Kingdom",
-  "Germany",
-  "Italy",
-  "France",
-  "China",
-  "India",
-  "Russia",
-  "Brazil"
-)
-
-bgcol <- "grey90"
-
-p0 <- growth_comps |> 
-  filter(country %in% selected) |> 
-  mutate(country = factor(country, levels = selected[length(selected):1])) |> 
-  mutate(text_col = ifelse(growth < 0, "negative", "positive")) |> 
-  ggplot(aes(x = growth, y = country, fill = edition)) +
-  geom_col(position = "dodge", width = 0.75, colour = bgcol) +
-  geom_text(aes(label = percent(growth, accuracy = 0.1), colour = text_col), 
-            position = position_dodge(width = 0.7), hjust = 1, vjust = 0.5,
-            fontface = "bold", size = 3) +
-  scale_fill_manual(values = c("darkblue", "steelblue"), guide = guide_legend(reverse = TRUE)) +
-  scale_colour_manual(values = c("positive" = "white", "negative" = "black")) +
-  scale_x_continuous(label = percent) +
-  geom_vline(xintercept = 0) +
-  guides(colour = "none") +
-  theme_minimal() +
-  theme(panel.grid = element_blank(),
-        plot.background = element_rect(fill = bgcol, colour = NA),
-        axis.line.y = element_blank(),
-        axis.text.x = element_blank(),
-        legend.position = c(0.8, 0.8)) +
-  labs(fill = "",
-       x = "",
-       y = "",
-       title = "Projected real GDP growth in 2025 for selected nations",
-       subtitle = "Forecasts for 2025, made in October 2024 and April 2025 in the IMF World Economic Outlook.
-The update for some, but not all, countries in January 2025 is not considered here.",
-       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
-
-
-
-p1 <- growth_comps |> 
-  filter(country %in% pacific) |> 
-  mutate(country = fct_reorder(country, -growth, .fun = mean)) |> 
-  mutate(text_col = ifelse(growth < 0, "negative", "positive")) |> 
-  ggplot(aes(x = growth, y = country, fill = edition)) +
-  geom_col(position = "dodge", width = 0.75, colour = bgcol) +
-  geom_text(aes(label = percent(growth, accuracy = 0.1), colour = text_col), 
-            position = position_dodge(width = 0.7), hjust = 1, vjust = 0.4,
-            fontface = "bold", size = 3) +
-  scale_fill_manual(values = c("darkblue", "steelblue"), guide = guide_legend(reverse = TRUE)) +
-  scale_colour_manual(values = c("positive" = "white", "negative" = "black")) +
-  scale_x_continuous(label = percent) +
-  geom_vline(xintercept = 0) +
-  guides(colour = "none") +
-  theme_minimal() +
-  theme(panel.grid = element_blank(),
-        plot.background = element_rect(fill = bgcol, colour = NA),
-        axis.line.y = element_blank(),
-        axis.text.x = element_blank(),
-        legend.position = c(0.8, 0.8)) +
-  labs(fill = "",
-       x = "",
-       y = "",
-       title = "Projected real GDP growth in 2025 for Pacific IMF members",
-       subtitle = "Forecasts for 2025, made in October 2024 and April 2025 in the IMF World Economic Outlook.",
-       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
-
-svg_png(p0, "../img/00288-selected-growth", w = 8, h = 7)
-
-svg_png(p1, "../img/00288-pict-growth", w = 8, h = 7)
-
-
-gcw <- growth_comps |> 
-  spread(edition, growth) |> 
-  mutate(is_pict = ifelse(country %in% pacific, "Pacific", "Other"))
-  
-
-p2 <- gcw |> 
-  ggplot(aes(x = `WEO October 2024`, y = `WEO April 2025`, colour = is_pict)) +
-  geom_abline(slope = 1, intercept = 0, colour = "grey75") +
-  geom_point() +
-  geom_text_repel(data = filter(gcw,
-                                `WEO April 2025` > 0.10 |
-                                 `WEO October 2024` < 0 |
-                                  `WEO October 2024` > 0.08 | 
-                                  is_pict == "Pacific"),
-                  aes(label = country),
-                  seed = 123) +
-  theme(legend.position = "none") +
-  coord_equal() +
-  scale_x_continuous(label = percent) +
-  scale_y_continuous(label = percent) +
-  scale_colour_manual(values = c("Pacific" = "blue", "Other" = "grey60")) +
-  annotate("text", x = 0.2, y = 0.04, label = "Forecast lower in April 2025", fontface = "italic") +
-  annotate("text", x = 0, y = 0.12, label = "Forecast higher in April 2025", fontface = "italic") +
-  labs(x = "Forecast as at October 2024",
-       y = "Forecast as at April 2025",
-       title = "Changes in IMF forecasts of real GDP growth for 2025, by country",
-       subtitle = "Pacific island countries highlighted. Diagonal line shows the same forecast in both 2024 and 2025.",
-       caption = "Source: IMF World Economic Outlooks. Growth rates based on constant prices, but are not per capita.")
-
-
-# note tried transform_modulus but it seems not to work?
-
-svg_png(p2, "../img/00288-pict-growth-scatter", w = 8, h = 6)
 
