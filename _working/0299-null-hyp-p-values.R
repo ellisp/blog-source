bf9a5f85ad7389834ad8d09d6654e87046e4309b
Peter Ellis <petere@spc.int>
2025-10-21 08:30:18 +1300

    tidying up p value null hypothesis distribution

--- _working/0299-null-hyp-p-values.R
+++ _working/0299-null-hyp-p-values.R
@@ -1,6 +1,10 @@
 
 # inspired by Richard McElreath tweet (reposted on Bluesky at
 # https://bsky.app/profile/rmcelreath.bsky.social/post/3lvx73m6jc22o)
+#
+# This is all about the fact that p-values under a null hypothesis don't
+# have a uniform distribution when the test under consideration has some 
+# strong discrete element.
 
 
 library(tidyverse)
@@ -25,82 +29,94 @@ tough_fisher <- function(k1, n1, k2, n2, set_both_zero = 1, ...){
 
 
 
-for(reps in c(1e3, 1e7)){
-
-  d <- expand_grid(
-    prob = rep(c(0.2, 0.5, 0.8), each = reps),
-    size = c(10, 100, 1000, NA)
-  ) |> 
-    mutate(size_lab = ifelse(is.na(size), "n=Pois(100)", glue("n={size}")),
-           prob_lab = glue("Prob={prob}")) |> 
-    mutate(size = ifelse(is.na(size), rpois(n(), 100), size)) |> 
-    mutate(x1 = rbinom(n(), size = size, prob = prob),
-          x2 = rbinom(n(), size = size, prob = prob),
-          # observed proportions p1 and p2 from the two populations
-          p1 = x1 / size,
-          p2 = x2 / size,
-          # under null hypothesis, the equal probability of both pops:
-          pmid = (p1 + p2) / 2,
-          # observed difference of the two proportions:
-          delt = abs(p1 - p2),
-          # variance of each of p1 and p2: p(1-p)/n
-          s1 = pmid * (1 - pmid) / size,
-          # standard deviation of the sum of two of those variances
-          sddelt = sqrt(s1 + s1)) |> 
-    # calculate p values
-    mutate(pval_hand = 2 * (1 - pnorm(delt / sddelt)),
-           pval_fisher = tough_fisher(x1, size, x2, size),
-           pval_proptest = NA)
-
-  if(reps < 10000){
-    system.time({
-      for (i in 1:nrow(d)){
-        x <- d[i, ]
-        d[i, "pval_proptest"] <- prop.test(c(x$x1, x$x2), c(x$size, x$size))$p.value
+# Takes about 30 seconds with 100,000 reps, but a long time with 10 million reps
+st <- system.time({
+  for(reps in c(1e3, 1e5, 1e7)){
+    set.seed(42)
+    
+    d <- expand_grid(
+      prob = rep(c(0.2, 0.5, 0.8), each = reps),
+      size = c(10, 100, 1000, NA)
+    ) |> 
+      mutate(size_lab = ifelse(is.na(size), "n=Pois(100)", glue("n={size}")),
+             prob_lab = glue("Prob={prob}")) |> 
+      mutate(size = ifelse(is.na(size), rpois(n(), 100), size)) |> 
+      mutate(x1 = rbinom(n(), size = size, prob = prob),
+            x2 = rbinom(n(), size = size, prob = prob),
+            # observed proportions p1 and p2 from the two populations
+            p1 = x1 / size,
+            p2 = x2 / size,
+            # under null hypothesis, the equal probability of both pops:
+            pmid = (p1 + p2) / 2,
+            # observed difference of the two proportions:
+            delt = abs(p1 - p2),
+            # variance of each of p1 and p2: p(1-p)/n
+            s1 = pmid * (1 - pmid) / size,
+            # standard deviation of the sum of two of those variances
+            sddelt = sqrt(s1 + s1)) |> 
+      # calculate p values
+      mutate(pval_hand = 2 * (1 - pnorm(delt / sddelt)),
+             pval_fisher = tough_fisher(x1, size, x2, size),
+             pval_proptest = NA)
+  
+    if(reps < 10000){
+      # when the number of reps is fairly small, I draw a pairs plot just to
+      # compare the different ways of calculating p values:
+      # - prop.test (out of the box R method). I couldn't find an easy way to vectorize this, 
+      #   is why it is only done here, via a loop. when reps is small
+      # - pval_hand (my home made approximate method)
+      # - pval_fisher (Fisher exact test, but toughened up as above to give 1 when both k1 and k2 are 0)
+      # The main conclusion from this is that my by-hand aproximation isn't great!
+      system.time({
+        for (i in 1:nrow(d)){
+          x <- d[i, ]
+          d[i, "pval_proptest"] <- prop.test(c(x$x1, x$x2), c(x$size, x$size))$p.value
+        }
+      })
+      # 5 seconds for reps=1000
+      
+      plot1 <- function(){
+        d |> 
+          select(pval_hand:pval_proptest, size_lab) |> 
+          ggpairs() |> 
+          print()
       }
-    })
-    # 5 seconds for reps=1000
-    
-    plot1 <- function(){
-      d |> 
-        select(pval_hand:pval_proptest, size_lab) |> 
-        ggpairs() |> 
-        print()
+      
+      svg_png(plot1, glue("../img/0299-pairs-{reps}"), w = 10, h = 8)      
     }
-    
-    svg_png(plot1, "../img/0299-pairs", w = 10, h = 8)      
-  }
-
-
-  plot2 <- d |> 
+  
+  
+    plot2 <- d |> 
+      ggplot(aes(x = pval_fisher)) +
+      facet_grid(size_lab  ~ prob_lab, scales = "free_y") +
+      geom_histogram(fill = "steelblue") +
+      scale_y_continuous(label = comma) +
+      labs(title = "Distribution of all p values when a null hypothesis is true",
+          subtitle = "Equal size binomial samples drawn from two populations with same underying probability",
+          x = "P value from Fisher's exact test
+    (when zero positive cases in both samples, p value is set to one)",
+          y = glue("Count of simulations (out of {comma(reps)})"))
+  
+    plot3 <- d |>
+    filter(pval_fisher < 0.05) |> 
     ggplot(aes(x = pval_fisher)) +
     facet_grid(size_lab  ~ prob_lab, scales = "free_y") +
     geom_histogram(fill = "steelblue") +
     scale_y_continuous(label = comma) +
-    labs(title = "Distribution of all p values when a null hypothesis is true",
-        subtitle = "Equal size binomial samples drawn from two populations with same underying probability",
-        x = "P value from Fisher's exact test
+    labs(title = "Distribution of significant (<0.05) p values when a null hypothesis is true",
+         subtitle = "Equal size binomial samples drawn from two populations with same underying probability",
+         x = "P value from Fisher's exact test
   (when zero positive cases in both samples, p value is set to one)",
-        y = glue("Count of simulations (out of {comma(reps)})"))
-
-  plot3 <- d |>
-  filter(pval_fisher < 0.05) |> 
-  ggplot(aes(x = pval_fisher)) +
-  facet_grid(size_lab  ~ prob_lab, scales = "free_y") +
-  geom_histogram(fill = "steelblue") +
-  scale_y_continuous(label = comma) +
-  labs(title = "Distribution of significant (<0.05) p values when a null hypothesis is true",
-       subtitle = "Equal size binomial samples drawn from two populations with same underying probability",
-       x = "P value from Fisher's exact test
-(when zero positive cases in both samples, p value is set to one)",
-       y = glue("Count of simulations (out of {comma(reps)})"))
-
-    
-  if(reps > 1000){
-    svg_png(plot2, "../img/0299-histogram", w = 9, h = 6)     
-    svg_png(plot3, "../img/0299-histogram-sig-only", w = 9, h = 6)      
+         y = glue("Count of simulations (out of {comma(reps)})"))
+  
+      
+    svg_png(plot2, glue("../img/0299-histogram-{reps}"), w = 9, h = 6)     
+    svg_png(plot3, glue("../img/0299-histogram-sig-only-{reps}"), w = 9, h = 6)      
+  
   }
-}
+})
+
+print(st)
 
 d |> 
   group_by(prob_lab, size_lab) |> 
