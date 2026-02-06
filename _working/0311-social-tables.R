1de6e6a6d2ec8c5c8213c7a346c8d94363cc1b07
Peter Ellis <petere@spc.int>
2026-02-07 10:06:52 +1100

    good version of quesnay scatter plot

--- _working/0311-social-tables.R
+++ _working/0311-social-tables.R
@@ -60,42 +60,42 @@ p1 <- d1 |>
                      breaks = c(0, d1$pop_prop), labels = c(0, d1$population),
                      sec.axis = dup_axis(breaks = c(0, 1/2.7, d1$inc_prop), 
                                          labels = c(0, "1.0", d1$income), 
-                                         name = "Income relative to the mean")) +
+                                         name = "Income relative to the mean (1.0)")) +
   labs(x = "",
        y = "Percentage of poulation",
        title = "Contemporary understanding of income inequality in France in the time of Louis XV",
        subtitle = "Factoral income distribution in *La philosophie rurale* by Mirabeau and Quesnay, 1763",
-       caption = "Data from Table 1.1 of Milanovic's *Visions of Inequality*, and plot style adapted from the same book.") +
-  theme_minimal(base_family = "Roboto") +
+       caption = "Quesnay's original data, reproduced in Table 1.1 of Milanovic's *Visions of Inequality*, and plot style adapted from Milanovic's.") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        panel.grid.minor = element_blank(),
         panel.grid.major = element_blank(),
         axis.line.y.right = element_line(colour = inc_col),
         axis.line.y.left = element_line(colour = pop_col),
         axis.title.y.left = element_text(colour = pop_col),
         axis.title.y.right = element_text(colour = inc_col),
         axis.text.y.left = element_text(colour = pop_col),
-        axis.text.y.right = element_text(colour = inc_col),
-        plot.subtitle = element_markdown(),
-        plot.caption = element_markdown(colour = "grey50"),
-        plot.title = element_markdown(family = "Sarala"))
+        axis.text.y.right = element_text(colour = inc_col))
 
 svg_png(p1, "../img/0311-quesnay-bar", w = 10, h = 6)
         
 #---------------scatter plot--------------
 
 
-d1 |> 
+p2 <- d1 |> 
   ggplot(aes(x = population, y = income, label = class_detail)) +
+  geom_hline(yintercept = 1, linetype = 2, colour = "grey80") +
   geom_point(size = 2) +
   geom_text_repel(colour = "steelblue", seed = 123, hjust = 0) +
-  scale_x_continuous(breaks = d1$population) +
-  scale_y_continuous(breaks = c(1, d1$income)) +
-  labs(y = "Income relative to the mean",
+  annotate("text", x = 30, y = 1.1, label = "Average income", colour = "grey80") +
+  scale_x_continuous(breaks = c(0, d1$population), limits = c(0, 50), expand = c(0,0 )) +
+  scale_y_continuous(breaks = c(0, 1, d1$income), limits = c(0, 3), expand = c(0,0 )) +
+  labs(y = "Income relative to the mean (1.0)",
      x = "Percentage of population",
      title = "Contemporary understanding of income inequality in France in the time of Louis XV",
      subtitle = "Factoral income distribution in *La philosophie rurale* by Mirabeau and Quesnay, 1763",
-     caption = "Data from Table 1.1 of Milanovic's *Visions of Inequality*.") +
-  theme(panel.grid.major = element_line())
+     caption = "Quesnay's original data, reproduced in Table 1.1 of Milanovic's *Visions of Inequality*.") +
+  theme(panel.grid.major = element_blank(),
+        axis.line = element_line(colour = "grey80"))
+
+svg_png(p2, "../img/0311-quesnay-scatter", w = 10, h = 6)
 
   
