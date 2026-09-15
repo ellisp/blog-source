5a8deadec550ee846477f4cfc978d7adcb333904
Peter Ellis <peter.ellis2013nz@gmail.com>
2026-09-15 15:25:08 +1100

    more or less final version of central limit theorem simulations code

--- _working/0332-clt-sims.R
+++ _working/0332-clt-sims.R
@@ -1,49 +1,108 @@
 library(nortest)
 library(tidyverse)
+library(actuar)
+library(glue)
 
 set.seed(123)
 
-sim_clt <- function(x, n = 30, reps = 5000, replace = TRUE, plot = TRUE){
-  means <- replicate(reps, mean(sample(x = x, size = n, replace = replace)))
+reps <- 10000
+sim_clt <- function(x, n = 30, reps = reps, replace = TRUE, plot = TRUE, conf = 0.95){
+  true_mean <- mean(x)
+  
+  samples <- replicate(reps, sample(x = x, size = n, replace = replace), simplify = FALSE)
+  means <- sapply(samples, mean)
+  
+  covered <- sapply(samples, function(s) {
+    ci <- t.test(s, conf.level = conf)$conf.int
+    ci[1] <= true_mean & true_mean <= ci[2]
+  })
+  
   if (plot){
     qqnorm(means)
     grid()
   }
-  return(nortest::ad.test(means)$statistic)
+  
+  return(list(
+    ad_stat  = nortest::ad.test(means)$statistic,
+    coverage = mean(covered)
+  ))
 }
 
 
 N <- 1e6
-ns <- c(5:30, 50, 100, 300, 1000, 2000, 5000, 10000)
+ns <- c(5:30, 50, 100, 300, 1000, 2000, 5000, 10000, 20000, 40000)
 sims <- list()
 
 pop_dists <- list(
-  "exp(rnorm(N))"                   = function() exp(rnorm(N)),
-  "rnorm(N)"                        = function() rnorm(N),
-  "rcauchy(N)"                      = function() rcauchy(N),
-  "rexp(N)"                         = function() rexp(N),
-  "c(rexp(N), exp(rnorm(N)))"       = function() c(rexp(N), exp(rnorm(N))),
-  "c(rnorm(N,0,1), rnorm(N,0.5,2))" = function() c(rnorm(N, 0, 1), rnorm(N, 0.5, 2))
+  "exp(rnorm(N))"                            = function() exp(rnorm(N)),
+  "exp(rnorm(N, sd = 2))"                    = function() exp(rnorm(N, sd = 2)),
+  "rnorm(N)"                                 = function() rnorm(N),
+  "rcauchy(N)"                               = function() rcauchy(N),
+  "rpareto(N, shape = 2.1, scale = 1)"       = function() rpareto(N, shape = 2.1, scale = 1),
+  "rexp(N)"                                  = function() rexp(N),
+  "c(rexp(N*0.6), exp(rnorm(N*0.4)))"        = function() c(rexp(N*0.6), exp(rnorm(N*0.4))),
+  "c(rnorm(N*0.9,0,1), rnorm(N*0.1,3,3))"    = function() c(rnorm(N*0.9,0,1), rnorm(N*0.1,3,3))
 )
 
 
 all_sims <- lapply(names(pop_dists), function(lbl) {
   pop <- pop_dists[[lbl]]()
+  
+  results <- lapply(ns, function(n) sim_clt(pop, n = n, plot = FALSE))
+  
   tibble(
     n = ns,
-    ad_pval = sapply(ns, function(n) sim_clt(pop, n = n, plot = FALSE)),
+    ad_stat  = sapply(results, `[[`, "ad_stat"),
+    coverage = sapply(results, `[[`, "coverage"),
     pop_dist = lbl
   )
 }) |> bind_rows()
 
+normal_band <- all_sims |> 
+  filter(pop_dist == "rnorm(N)") |> 
+  pull(ad_stat) |> 
+  quantile(probs = c(0.025, 0.975))
 
-all_sims |> 
-  ggplot(aes(x = n, y = ad_pval))+
-  facet_wrap(~pop_dist)+
-  geom_hline(yintercept = 0.05, colour = "steelblue") +
+p1 <- all_sims |> 
+  mutate(pop_dist = fct_reorder(pop_dist, ad_stat)) |> 
+  ggplot(aes(x = n, y = ad_stat))+
+  facet_wrap(~pop_dist, nrow = 2)+
+  annotate("rect", xmin = min(ns), xmax = Inf, ymin = normal_band[1], ymax = normal_band[2], alpha = 0.5, fill = "orange")+
   geom_point()+
-  scale_x_log10(label = comma) +
+  scale_x_log10(label = comma, breaks = ns[ns >=30 | ns %in% c(5, 10, 20)]) +
   scale_y_log10(label = comma)+
-  theme(panel.grid.minor.y = element_blank())
+  theme(panel.grid.minor = element_blank(), 
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.text = element_text(face = "plain", size = 9)) +
+  labs(x = "Sample size",
+       y = "Anderson-Darling statistic",
+       subtitle = glue("Points represent average from {comma(reps)} simulations of given sample size. Shaded area covers 95% of values from a normal distribution."),
+       title = "Increasing sample size and growing effectiveness of the central limit theorem - Anderson-Darling statistic")
 
-# thoughts - test coverage of a t distribution confidence interval rather than the AD statistic (or as well as)
+svg_png(p1, "../img/0332-ad-stat", w = 10, h = 6)
+
+
+
+p2 <- all_sims |> 
+  mutate(pop_dist = fct_reorder(pop_dist, ad_stat)) |> 
+  ggplot(aes(x = n, y = coverage))+
+  facet_wrap(~pop_dist, nrow = 2) +
+  annotate("rect", xmin = min(ns), xmax = Inf, ymin = 0.95, ymax = 1, alpha = 0.5, fill = "orange")+
+  geom_point()+
+  scale_x_log10(label = comma, breaks = ns[ns >=30 | ns %in% c(5, 10, 20)]) +
+  scale_y_continuous(label = percent)+
+  theme(panel.grid.minor = element_blank(), 
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.text = element_text(face = "plain", size = 9)) +
+  labs(x = "Sample size",
+       y = "Coverage of a 95% confidence interval based on t distribution",
+       subtitle = glue("Points represent average from {comma(reps)} simulations of given sample size. Shaded area shows 95% and higher, as desired."),
+       title = "Increasing sample size and growing effectiveness of the central limit theorem - confidence interval coverage")
+
+svg_png(p2, "../img/0332-coverage", w = 10, h = 6)
+
+p3 <- p2 +
+  labs(y = "Coverage of a 95% confidence interval based on t distribution\nY axis truncated to only start at 80%; some points excluded because of that.") +
+  coord_cartesian(ylim = c(0.8, 1))
+
+svg_png(p3, "../img/0332-coverage-trunc", w = 10, h = 6)
